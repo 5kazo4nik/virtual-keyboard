39cd31266b095cc22265b8e6ef010f524f29af3f
Oleg <alhimov.oleg2012@yandex.ru>
2023-04-19 13:32:10 +0300

    feat: create webpack config file, html file and some folders

---
+++ virtual-keyboard/webpack.config.js
@@ -0,0 +1,100 @@
+const webpack = require('webpack');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+module.exports = (env, options) => {
+  const isProduction = options.mode === 'production';
+  const filename = (ext) => (isProduction ? `[name].[contenthash].${ext}` : `[name].${ext}`);
+  const optimization = () => {
+    const configObj = {
+      splitChunks: {
+        chunks: 'all',
+      },
+    };
+    if (isProduction) {                                      // Может не работать
+      configObj.minimizer = [
+        new CssMinimizerWebpackPlugin(),
+        new TerserWebpackPlugin()
+      ]
+    }
+    return configObj;
+  };
+
+  const config = {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? false : 'source-map',
+    watch: !isProduction,
+    watchOptions: {
+      poll: true,
+      ignored: /node_modules/,
+    },
+    optimization: optimization(),
+    devServer: {
+      historyApiFallback: true,
+      contentBase: path.resolve(__dirname, 'app'),
+      open: true,
+      compress: true,
+      hot: true,
+      port: 3000,
+    },
+    entry: './source/scripts/index.js',
+    output: {
+      path: path.resolve(__dirname, 'app'),
+      filename: `./scripts/${filename('js')}`,
+      publicPath: '',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+        {
+          test: /\.html$/i,
+          loader: 'html-loader',
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                hmr: !isProduction,                                           // Может не работать
+                publicPath: (resourcePath, context) => {
+                  return path.relative(path.dirname(resourcePath), context) + '/';
+                },
+              },
+            },
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: `./css/${filename('css')}`,
+      }),
+      new HtmlWebpackPlugin({
+        template: './pages/index.html',
+        // filename: 'index.html',
+        filename: filename('html'),
+        minify: {
+          collapseWhitespace: isProduction ? true : false,
+        },
+      }),
+    ],
+  };
+  return config;
+};
